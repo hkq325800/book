80c85d6819e470eb0f99cea55401b95c9b21176d
hkq325800 <hkq325800@163.com>
2014-11-11 10:51:06 +0800

    乌龙2

--- conn.php
+++ conn.php
@@ -9,8 +9,7 @@ $con = mysql_connect("localhost", "mengma", "mengma");//mysql_connect("localhost
 		mysql_select_db("library", $con);
 		//返回json0
 	function database_error(){
-		$response[0]= array('status'=>"0");
-		$response[1]= array('info'=>"数据库连接失败");
+		$response[0]= array('status'=>"0",'info'=>"数据库连接失败");
 		$response = json_encode($response);
 		echo $response;
 	}
