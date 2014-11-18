938fb8b424be920c55dffaf238b5d4ad23a6e0a9
TODO <hkq325800@163.com>
2014-11-18 12:10:51 +0800

    乌龙5

--- API.php
+++ API.php
@@ -349,7 +349,7 @@
 		}
 		else{//搜索
 			!$flag?
-			$where=" where book_status in ('已被借','未被借') and $xtype like '%$xkeyword%' "：
+			$where=" where book_status in ('已被借','未被借') and $xtype like '%$xkeyword%' ":
 			$where=" where $xtype like '%$xkeyword%' ";
 		}
 		$sql=$sql.$where;
